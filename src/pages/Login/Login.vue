3bea8ec6393e5a1c1460dadf57adf60a8bee8017
etech <devkant@excellencetechnologies.in>
2018-12-12 16:03:53 +0530

    login Component added

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -1,68 +1,59 @@
 <template>
   <div class="login-page">
-    <b-container>
-      <h5 class="logo">
-        <i class="fa fa-circle text-gray" />
-        sing
-        <i class="fa fa-circle text-warning" />
-      </h5>
-      <Widget class="mx-auto" title="<h3 class='mt-0'>Login to your Web App</h3>" customHeader>
-        <p class="text-muted mb-0 mt fs-sm">
-          Use Facebook, Twitter or your email to sign in.
-        </p>
-        <p class="text-muted fs-sm">
-          Don't have an account? Sign up now!
-        </p>
-        <form class="mt" @submit.prevent="login">
-          <b-alert class="alert-sm" variant="danger" :show="!!errorMessage">
-            {{errorMessage}}
-          </b-alert>
+    <b-container class="pt-1 pb-1 bg-white shadow w-75">
+      <Widget class="mx-auto" customHeader>
+        <h5 class="logo mb-5">
+          <img src="./logo.png" width="100%" alt="logo">
+        </h5>
+        <h6 class="mt-0 mb-5 text-center font-weight-bold">Enter Your ExcellenceHR Username to Login</h6>
+        <form class="mt-4" @submit.prevent="login">
+          <b-alert class="alert-sm" variant="danger" :show="!!errorMessage">{{errorMessage}}</b-alert>
           <div class="form-group">
-            <input class="form-control no-border" ref="username"
-              required type="text" name="username" placeholder="Username" />
+            <input
+              class="form-control no-border"
+              ref="username"
+              required
+              type="text"
+              name="username"
+              placeholder="Username"
+            >
           </div>
           <div class="form-group">
-            <input class="form-control no-border" ref="password"
-            required type="password" name="password" placeholder="Password" />
+            <input
+              class="form-control no-border"
+              ref="password"
+              required
+              type="password"
+              name="password"
+              placeholder="Password"
+            >
           </div>
           <div class="clearfix">
+            <div class="abc-checkbox float-left">
+              <input type="checkbox" id="checkbox">
+              <label for="checkbox" class="text-muted fs-sm">
+                <span class="align-text-middle">Keep me signed in</span>
+              </label>
+            </div>
             <div class="btn-toolbar float-right">
-              <b-button type="reset" size="sm" variant="default">Create an Account</b-button>
-              <b-button type="submit" size="sm" variant="inverse">Login</b-button>
-            </div>
-          </div>
-          <div class="row no-gutters mt-3">
-            <div class="col">
-              <div class="abc-checkbox">
-                <input
-                  type="checkbox"
-                  id="checkbox"
-                />
-                <label for="checkbox" class="text-muted fs-sm">Keep me signed in</label>
-              </div>
-            </div>
-            <div class="col">
-              <a class="mt-sm" href="">Trouble with account?</a>
+              <b-button class="pr-4 pl-4" type="submit" size="sm" variant="inverse">Login</b-button>
             </div>
           </div>
         </form>
       </Widget>
     </b-container>
-    <footer class="footer">
-      2017 &copy; Sing. Admin Dashboard Template.
-    </footer>
   </div>
 </template>
 
 <script>
-import Widget from '@/components/Widget/Widget';
+import Widget from "@/components/Widget/Widget";
 
 export default {
-  name: 'LoginPage',
+  name: "LoginPage",
   components: { Widget },
   data() {
     return {
-      errorMessage: null,
+      errorMessage: null
     };
   },
   methods: {
@@ -71,17 +62,17 @@ export default {
       const password = this.$refs.password.value;
 
       if (username.length !== 0 && password.length !== 0) {
-        window.localStorage.setItem('authenticated', true);
-        this.$router.push('/app/main/analytics');
+        window.localStorage.setItem("authenticated", true);
+        this.$router.push("/app/main/analytics");
       }
-    },
-  },
-  created() {
-    if (window.localStorage.getItem('authenticated') === 'true') {
-      this.$router.push('/app/main/analytics');
     }
   },
+  created() {
+    if (window.localStorage.getItem("authenticated") === "true") {
+      this.$router.push("/app/main/analytics");
+    }
+  }
 };
 </script>
 
-<style src="./Login.scss" lang="scss" scoped />
+<style src="./Login.scss" lang="scss" scoped >
