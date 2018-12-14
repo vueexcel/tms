53c723439a1fecc62314bd5ebc0a897ff53869fa
etech <devkant@excellencetechnologies.in>
2018-12-14 16:42:33 +0530

    added vuex and pathify

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,18 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import pathify from './pathify'
+import login from './modules/login'
 
 import layout from './layout';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  plugins: [
+    pathify.plugin
+  ],
   modules: {
     layout,
-  },
+    login
+  }
 });
