f5166eebc7ea4156a0210c9b469c917d2ba62e0b
etechnidhi <nidhi_sharma@excellencetechnologies.in>
2019-01-29 19:14:46 +0530

    edit the employee

--- src/components/Employee/employeeWidget.vue
+++ src/components/Employee/employeeWidget.vue
@@ -3,19 +3,40 @@
     <div class="pb-xlg mb-0">
       <div class="h-100 bg-white p-2 border-round">
         <div class="float-right">
-          <i class="fa fa-pencil text-light" aria-hidden="true"></i>
+          <i class="fa fa-pencil text-muted btn" v-if="!edit" @click="editEmployee(employee)" aria-hidden="true"></i>
+          <div class="text-center" v-else>
+            <a href="#" class="btn btn-rounded-f button-for-employee" @click="saveEdit()">
+              <div class="text-gray" style="font-size: 12px;">SAVE</div>
+            </a>
+          </div>
         </div>
         <div class="text-center pr-3 mt-2">
-          <img class="rounded-circle" src="@/assets/people/a5.jpg" width="75" height="75" alt="...">
+          <img
+            class="rounded-circle ml-5"
+            src="@/assets/people/a5.jpg"
+            width="75"
+            height="75"
+            alt="..."
+          >
         </div>
 
         <div class="pt-2 text-center">
           <span class="fs-larger text-capitalize">
-            <span>{{employee.name}}</span>
+            <span class="employee-name" v-if="!edit">{{employee.name}}</span>
+            <div class="form-group" v-else>
+              <input type="text" class="border border-name text-secondary" id="name" v-model="name">
+            </div>
           </span>
-          <p class="fw-small text-primary">{{employee.technology}}</p>
+          <span>
+            <p
+              class="fw-small text-primary employee-technology"
+              v-if="!edit"
+            >{{employee.technology}}</p>
+              <b-form-select v-else v-model="technology" :options="options" class="mb-3">
+              </b-form-select>
+          </span>
         </div>
-        <div class="text-center">
+        <div class="text-center mb-3">
           <a href="#" class="btn btn-rounded-f button-for-employee">
             <div class="text-gray" style="font-size: 12px;">{{employee.post}}</div>
           </a>
@@ -23,7 +44,7 @@
       </div>
       <div class="border-managers-container">
         <div class="ml-3">
-          <h6>MANAGERS</h6>
+          <p class="managers">MANAGERS</p>
         </div>
         <div class="pr-3 ml-3 mb-2 mt-2">
           <b-row v-if="employee && employee.manager" class="avatars">
@@ -44,8 +65,8 @@
                 <b-collapse
                   :id="'manager' +img.img_id"
                   v-if="manager.img_id === img.img_id"
-                  class="mt-2"
                 >
+                  class="mt-2"
                   <!-- <b-card> -->
                   <div style="display:flex">
                     <img
@@ -55,38 +76,47 @@
                       width="40"
                       height="40"
                     >
-                    <span>
-                      <p class="text-primary fw-semi-bold fs-larger">{{manager.name}}</p>
-                      <p class="text-light">{{manager.work}}</p>
+                    <i class="fa fa-times-circle" aria-hidden="true"></i>
+                    <span class="ml-3">
+                      <p class="text-primary fw-semi-bold fs-larger manager-name">{{manager.name}}</p>
+                      <p class="text-dark manager-work">{{manager.work}}</p>
                       <!-- <p class="text-primary fs-larger">{{manager}}</p> -->
                     </span>
                   </div>
                   <div>
-                    <p>Manager Weight:</p>
-                      <ul>
-  <li class="list"></li>
-  <li class="list"></li>
-  <li class="list"></li>
-  <li class="active list"></li>
-  <li class="list"></li>
-  <li class="list"></li>
-  <li class="list"></li>
-  <li class="list"></li>
-  <li class="list"></li>
-  <li class="list"></li>
-</ul>  
-<ul class="numbers">
-  <li class="text-primary fw-semi-bold">1</li>
-  <li class="text-primary fw-semi-bold">2</li>
-  <li class="text-primary fw-semi-bold">3</li>
-  <li class="text-primary fw-semi-bold">4</li>
-  <li class="text-primary fw-semi-bold">5</li>
-  <li class="text-primary fw-semi-bold">6</li>
-  <li class="text-primary fw-semi-bold">7</li>
-  <li class="text-primary fw-semi-bold">8</li>
-  <li class="text-primary fw-semi-bold">9</li>
-  <li class="text-primary fw-semi-bold">10</li>
-</ul>  
+                    <p class="mt-2 mb-2 text-dark">Manager Weight:</p>
+                    <ul class="progress-bar-employee">
+                      <li class="list">
+                        <div class="text-primary fw-semi-bold employee-progress">1</div>
+                      </li>
+                      <li class="list">
+                        <div class="text-primary fw-semi-bold employee-progress">2</div>
+                      </li>
+                      <li class="list">
+                        <div class="text-primary fw-semi-bold employee-progress">3</div>
+                      </li>
+                      <li class="active list">
+                        <div class="text-primary fw-semi-bold employee-progress">4</div>
+                      </li>
+                      <li class="list">
+                        <div class="text-primary fw-semi-bold employee-progress">5</div>
+                      </li>
+                      <li class="list">
+                        <div class="text-primary fw-semi-bold employee-progress">6</div>
+                      </li>
+                      <li class="list">
+                        <div class="text-primary fw-semi-bold employee-progress">7</div>
+                      </li>
+                      <li class="list">
+                        <div class="text-primary fw-semi-bold employee-progress">8</div>
+                      </li>
+                      <li class="list">
+                        <div class="text-primary fw-semi-bold employee-progress">9</div>
+                      </li>
+                      <li class="list">
+                        <div class="text-primary fw-semi-bold employee-progress">10</div>
+                      </li>
+                    </ul>
                     <p>All</p>
                   </div>
                   <!-- <b-card> -->
@@ -98,8 +128,8 @@
           </b-row>
         </div>
       </div>
-      <!-- </Widget> -->
     </div>
+      <!-- </Widget> -->
   </div>
 </template>
 
@@ -107,18 +137,25 @@
 import $ from "jquery";
 import "imports-loader?window.jQuery=jquery,this=>window!widgster"; // eslint-disable-line
 // import Widget from '@/components/Widget/Widget'
+import { get, call, sync } from "vuex-pathify";
+
 export default {
   name: "employeeWidget",
   data() {
     return {
       show: false,
-      manager: {}
+      manager: {},
+      edit: false
     };
   },
   props: {
     employee: { type: Object, default: () => ({}) }
   },
   computed: {
+    emp_arr: get("manageEmployee/employees"),
+    name:sync("manageEmployee/employeeName"),
+    technology: sync("manageEmployee/employeeTechnology"),
+    options: sync("manageEmployee/options"),
     randomId() {
       return Math.floor(Math.random() * 100);
     },
@@ -134,11 +171,25 @@ export default {
     }
   },
   methods: {
+    saveEmployeeInfo: call("manageEmployee/saveEmployeeInfo"),
     showCollapse(value) {
       this.show = !this.show;
       if (value) {
         this.manager = value;
       }
+    },
+    editEmployee(employee) {
+      this.edit = true;
+      this.name = employee.name
+      this.options[0].text = employee.technology
+    },
+    saveEdit() {
+      this.edit = false
+      this.saveEmployeeInfo({
+        name: this.name,
+        technology: this.technology,
+        id: this.employee.id
+      })
     }
   },
   // components: {
--- src/store/modules/manageEmployee.js
+++ src/store/modules/manageEmployee.js
@@ -1,149 +1,170 @@
 // import axios from 'axios'
-import {make} from 'vuex-pathify'
+import { make } from "vuex-pathify";
 
 const state = {
-    employees: [
+  employees: [
+    {
+      name: "Manish Prakash",
+      technology: "R.E.A.C.T",
+      post: "Sr.ReactJs Developer",
+      id: 1,
+      manager: [
         {
-            name: 'Manish Prakash',
-            technology: 'R.E.A.C.T',
-            post: 'Sr.ReactJs Developer',
-            id: 1,
-            manager: [{
-                image:'/img/a5.84f014f0.jpg',
-                img_id:'one',
-                name:'Mahima K',
-                work: 'VP Operations'
-            },
-            {
-                image:'/img/a3.f6dc73f2.jpg',
-                img_id:'two',
-                name: 'Manish',
-                work: 'CEO'
-            },
-            {
-                image:'/img/a6.2966ab8e.jpg',
-                img_id:'three',
-                name: 'Manish',
-                work: 'CEO'
-            },
-            {
-                image:'/img/a3.f6dc73f2.jpg',
-                img_id:'four',
-                name: 'Arun',
-                work: 'Sr. MEAN Stack Developer'
-            },
-            {
-                image:'/img/a3.f6dc73f2.jpg',
-                img_id:'five',
-                name: 'Deepak',
-                work: 'Sr. Magento Developer'
-            },
-            {
-                image:'/img/a6.2966ab8e.jpg',
-                img_id:'six',
-                name: 'Mahima k',
-                work: 'VP Operations'
-            }
-            ]
+          image: "/img/a5.84f014f0.jpg",
+          img_id: "one",
+          name: "Mahima K",
+          work: "VP Operations"
         },
         {
-            name: 'Manish Prakash',
-            technology: 'KPI Name',
-            post: 'ReactJs Developer',
-            id: 2,
-            manager: [
-                {
-                    image:'/img/a3.f6dc73f2.jpg',
-                    img_id:'seven',
-                    name: 'Arun',
-                    work: 'Sr. MEAN Stack Developer'
-                },
-                {
-                    image:'/img/a3.f6dc73f2.jpg',
-                    img_id:'eight',
-                    name: 'Deepak',
-                    work: 'Sr. Magento Developer'
-                },
-                {
-                    image:'/img/a6.2966ab8e.jpg',
-                    img_id:'nine',
-                    name: 'Mahima k',
-                    work: 'VP Operations'
-                }   
-            ]
+          image: "/img/a3.f6dc73f2.jpg",
+          img_id: "two",
+          name: "Manish",
+          work: "CEO"
         },
         {
-            name: 'Manish Prakash',
-            technology: 'KPI Name',
-            post: 'Project Manager',
-            id: 3,
-            manager: [
-                {
-                    image:'/img/a3.f6dc73f2.jpg',
-                    img_id:'ten',
-                    name: 'Arun',
-                    work: 'Sr. MEAN Stack Developer'
-                },
-            ]
+          image: "/img/a6.2966ab8e.jpg",
+          img_id: "three",
+          name: "Manish",
+          work: "CEO"
         },
         {
-            name: 'Manish Prakash',
-            technology: 'KPI Name',
-            post: 'ReactJs Developer',
-            id: 4,
-            manager: [
-                {
-                    image:'/img/a3.f6dc73f2.jpg',
-                    img_id:'eleven',
-                    name: 'Deepak',
-                    work: 'Sr. Magento Developer'
-                },
-                {
-                    image:'/img/a6.2966ab8e.jpg',
-                    img_id:'twelve',
-                    name: 'Mahima k',
-                    work: 'VP Operations'
-                }
-            ]
+          image: "/img/a3.f6dc73f2.jpg",
+          img_id: "four",
+          name: "Arun",
+          work: "Sr. MEAN Stack Developer"
         },
         {
-            name: 'Manish Prakash',
-            technology: 'KPI Name',
-            post: 'ReactJs Developer',
-            id: 2,
-            manager: [
-                {
-                    image:'/img/a3.f6dc73f2.jpg',
-                    img_id:'thirteen',
-                    name: 'Arun',
-                    work: 'Sr. MEAN Stack Developer'
-                },
-                {
-                    image:'/img/a3.f6dc73f2.jpg',
-                    img_id:'forteen',
-                    name: 'Deepak',
-                    work: 'Sr. Magento Developer'
-                },
-                {
-                    image:'/img/a6.2966ab8e.jpg',
-                    img_id:'fifteen',
-                    name: 'Mahima k',
-                    work: 'VP Operations'
-                }   
-            ]
+          image: "/img/a3.f6dc73f2.jpg",
+          img_id: "five",
+          name: "Deepak",
+          work: "Sr. Magento Developer"
+        },
+        {
+          image: "/img/a6.2966ab8e.jpg",
+          img_id: "six",
+          name: "Mahima k",
+          work: "VP Operations"
         }
-    ]
-}
-// const mutation = make.mutation(state)
+      ]
+    },
+    {
+      name: "Manish Prakash",
+      technology: "KPI Name",
+      post: "ReactJs Developer",
+      id: 2,
+      manager: [
+        {
+          image: "/img/a3.f6dc73f2.jpg",
+          img_id: "seven",
+          name: "Arun",
+          work: "Sr. MEAN Stack Developer"
+        },
+        {
+          image: "/img/a3.f6dc73f2.jpg",
+          img_id: "eight",
+          name: "Deepak",
+          work: "Sr. Magento Developer"
+        },
+        {
+          image: "/img/a6.2966ab8e.jpg",
+          img_id: "nine",
+          name: "Mahima k",
+          work: "VP Operations"
+        }
+      ]
+    },
+    {
+      name: "Manish Prakash",
+      technology: "KPI Name",
+      post: "Project Manager",
+      id: 3,
+      manager: [
+        {
+          image: "/img/a3.f6dc73f2.jpg",
+          img_id: "ten",
+          name: "Arun",
+          work: "Sr. MEAN Stack Developer"
+        }
+      ]
+    },
+    {
+      name: "Manish Prakash",
+      technology: "KPI Name",
+      post: "ReactJs Developer",
+      id: 4,
+      manager: [
+        {
+          image: "/img/a3.f6dc73f2.jpg",
+          img_id: "eleven",
+          name: "Deepak",
+          work: "Sr. Magento Developer"
+        },
+        {
+          image: "/img/a6.2966ab8e.jpg",
+          img_id: "twelve",
+          name: "Mahima k",
+          work: "VP Operations"
+        }
+      ]
+    },
+    {
+      name: "Manish Prakash",
+      technology: "KPI Name",
+      post: "ReactJs Developer",
+      id: 2,
+      manager: [
+        {
+          image: "/img/a3.f6dc73f2.jpg",
+          img_id: "thirteen",
+          name: "Arun",
+          work: "Sr. MEAN Stack Developer"
+        },
+        {
+          image: "/img/a3.f6dc73f2.jpg",
+          img_id: "forteen",
+          name: "Deepak",
+          work: "Sr. Magento Developer"
+        },
+        {
+          image: "/img/a6.2966ab8e.jpg",
+          img_id: "fifteen",
+          name: "Mahima k",
+          work: "VP Operations"
+        }
+      ]
+    }
+  ],
+  options: [
+    {value: null, text: ''},
+    { value: 'MAGENTO', text: 'MAGENTO' },
+    { value: 'REACT', text: 'REACT' },
+    { value: 'ANGULAR', text: 'ANGULAR' },
+    { value: 'VUE', text: 'VUE' },
+    { value: 'NODE', text: 'NODE' },
+    {value: 'LARAVEL', text: 'LARAVEL'}
+  ],
+  employeeName: '',
+  employeeTechnology: "",
+  employeePost: ""
+};
+const mutations = make.mutations(state);
 const actions = {
-    ...make.actions(state),
-}
-
+  ...make.actions(state),
+  saveEmployeeInfo({state,commit}, payload) {
+    state.employees.forEach(employee => {
+      if (employee.id === payload.id) {
+        employee.name = payload.name,
+        employee.technology = payload.technology
+      }
+      
+    });
+  }
+};
 
 //create store
-export default({
-    namespaced: true,
-    state,
-    // mutation,
-    actions
-})
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions
+};
