fea7295a145da1b50f3e9534b8d7c8ec76e80b66
arun <arun.etech2011@gmail.com>
2019-02-23 12:05:35 +0530

    checkin divided into more smaller components

---
+++ src/pages/Checkin/CheckinComponents/StandupReportWidget.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="content p-0">
+    <p>
+      <img
+        class="rounded-circle mr-2 mt-1 float-left"
+        src="@/assets/people/a6.jpg"
+        width="35"
+        height="35"
+        alt="..."
+      >
+      <span>
+        <span class="text-primary fs-larger fw-semi-bold">Jessica Smith</span>
+        <br>
+        <span>December 13,2018 at 8:03 PM</span>
+      </span>
+    </p>
+    <p>#report
+      <br>worked on singapp
+      <br>created login component
+      <br>installed vuex
+      <br>worked on HMG
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style src="../Checkin.scss" lang="scss" scoped />
