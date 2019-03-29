280873a7e66748ffbd5715203f12abede8896413
vueexcel <vueexcel@excellencetechnologies.in>
2019-03-29 17:59:20 +0530

    changes

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,22 @@
 // vue.config.js
-// module.exports = {
-//     devServer: {
-//         proxy: 'http://192.168.1.102:8080/'
-//         // proxy: 'http://127.0.0.1:5000/'
-//         // proxy: 'http://localhost:8080'
-//     }
-// }
-
-
-
-
 module.exports = {
-    configureWebpack: {
-        devServer: {
-            clientLogLevel: 'info',
-            watchOptions: {
-                poll: true
-            }
-        }
+    devServer: {
+        proxy: 'http://192.168.1.102:8080/'
+        // proxy: 'http://127.0.0.1:5000/'
+        // proxy: 'http://localhost:8080'
     }
 }
+
+
+
+
+// module.exports = {
+//     configureWebpack: {
+//         devServer: {
+//             clientLogLevel: 'info',
+//             watchOptions: {
+//                 poll: true
+//             }
+//         }
+//     }
+// }
