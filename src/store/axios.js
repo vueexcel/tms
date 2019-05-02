68961db39ef5d5cbe38287ebf82330edd8f68f10
vueexcel <vueexcel@excellencetechnologies.in>
2019-05-02 17:46:18 +0530

    conflicts

--- src/store/axios.js
+++ src/store/axios.js
@@ -1,17 +1,10 @@
 import axios from "axios";
 // axios.defaults.baseURL = 'http://5.9.144.226:8000';
 // axios.defaults.baseURL = 'https://pure-wave-45439.herokuapp.com';
-<<<<<<< HEAD
-axios.defaults.baseURL = 'https://agile-hamlet-51923.herokuapp.com';
-
-axios.interceptors.request.use((config) => {
-    const token = localStorage.getItem('authenticated');
-=======
 axios.defaults.baseURL = "https://agile-hamlet-51923.herokuapp.com";
 axios.interceptors.request.use(
   config => {
     const token = localStorage.getItem("authenticated");
->>>>>>> c980eb86c8aeb4a7cfb1f0e5b68030aac43bc188
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
