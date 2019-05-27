5e078369c9fb41480ef1ef54dd9e5bfb5c585a3f
vueexcel <vueexcel@excellencetechnologies.in>
2019-05-27 14:15:22 +0530

    Slider on weekly report juniors

--- src/pages/WeeklyReportReview/WeeklyReport.vue
+++ src/pages/WeeklyReportReview/WeeklyReport.vue
@@ -14,7 +14,7 @@
             class="alert-transparent alert-danger mt-5"
           >{{errorMessage}}</b-alert>
         </div>
-        <b-row v-if="weeklyData.length">
+        <b-row v-if="weeklyData.length" class="employees">
           <b-col
             lg="2"
             md="4"
@@ -33,7 +33,7 @@
             />
           </b-col>
         </b-row>
-        <b-row v-else>
+        <b-row v-else class="employees">
           <b-col lg="2"  md="4" xs="12"   class="column" v-for="employee in allJuniors_" :key="employee._id">
             <WeeklyReviewComponent
               :employee="employee"
@@ -89,7 +89,9 @@ export default {
       allweeklyData: [],
       loading: false,
       error: false,
-      errorMessage: ""
+      errorMessage: "",
+        slide: 0,
+        sliding: null
     };
   },
   mounted() {
@@ -112,6 +114,12 @@ export default {
       this.activeId = employee._id;
       this.activeEmp = employee;
     },
+    onSlideStart(slide) {
+        this.sliding = true
+      },
+      onSlideEnd(slide) {
+        this.sliding = false
+      },
     fetchWeeklyReport() {
       // this.loading = true;
       // this.getWeeklyReport_()
