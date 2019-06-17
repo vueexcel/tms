1e43777f0a61e2bf99fb0e0079cc56ba8523a4f2
vueexcel <vueexcel@excellencetechnologies.in>
2019-06-17 15:26:34 +0530

    axios default

--- src/store/axios.js
+++ src/store/axios.js
@@ -1,9 +1,5 @@
 import axios from "axios";
-// axios.defaults.baseURL = 'http://5.9.144.226:8000';
-// axios.defaults.baseURL = 'https://pure-wave-45439.herokuapp.com';
-axios.defaults.baseURL = "https://frozen-thicket-11377.herokuapp.com";
-//http://176.9.137.77:8000
-// axios.defaults.baseURL = process.env.VUE_APP_ROOT_API
+axios.defaults.baseURL = process.env.VUE_APP_ROOT_API
 axios.interceptors.request.use(
   config => {
     const token = localStorage.getItem("authenticated") || $cookies.get("keepLoggedIn");
