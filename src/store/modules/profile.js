06fb99651a507d09b2154a47c12ec8d7ed6fc24c
etechnidhi <nidhi_sharma@excellencetechnologies.in>
2020-07-14 12:36:22 +0530

    comment router from profile.js

--- src/store/modules/profile.js
+++ src/store/modules/profile.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { make } from "vuex-pathify";
-import router from "./../../Routes";
+// import router from "./../../Routes";
 
 // setup store
 const state = {
@@ -11,41 +11,41 @@ const state = {
 const mutations = make.mutations(state);
 const actions = {
   ...make.actions(state),
-  async getProfile({ commit, dispatch }, payload) {
-    try {
-      let response = await axios.get("/auth/profile");
-      if (response) {
-        commit("user", response.data);
-        if (payload && payload.reload) {
-          return true;
-        } else {
-          if (response.data.role === "Admin") {
-            if (router.currentRoute.path !== '/admin/manageKpi') return router.push("/admin/manageKpi");
-          } else {
-            dispatch("getManagerReviewStatus");
-            if (payload === undefined) {
-              if (localStorage.getItem("weeklyAutomate")) {
-                router.push("/app/automateWeekly");
-              } else if (localStorage.getItem('updateReview') && localStorage.getItem('updateReview') === 'true') {
-                router.push('/app/week/WeeklyReport')
-              } else {
-                if (router.currentRoute.path !== '/app/profile') return router.push("/app/profile");
-              }
-            }
-          }
-        }
-        return true;
-      } else {
-        // commit('loginfailed', err)
-      }
-    } catch (error) {
-      console.log(error);
-      if (error && error.response.data.msg === "Token has expired") {
-        localStorage.removeItem("authenticated");
-        router.push("/");
-      }
-    }
-  },
+  // async getProfile({ commit, dispatch }, payload) {
+  //   try {
+  //     let response = await axios.get("/auth/profile");
+  //     if (response) {
+  //       commit("user", response.data);
+  //       if (payload && payload.reload) {
+  //         return true;
+  //       } else {
+  //         if (response.data.role === "Admin") {
+  //           if (router.currentRoute.path !== '/admin/manageKpi') return router.push("/admin/manageKpi");
+  //         } else {
+  //           dispatch("getManagerReviewStatus");
+  //           if (payload === undefined) {
+  //             if (localStorage.getItem("weeklyAutomate")) {
+  //               router.push("/app/automateWeekly");
+  //             } else if (localStorage.getItem('updateReview') && localStorage.getItem('updateReview') === 'true') {
+  //               router.push('/app/week/WeeklyReport')
+  //             } else {
+  //               if (router.currentRoute.path !== '/app/profile') return router.push("/app/profile");
+  //             }
+  //           }
+  //         }
+  //       }
+  //       return true;
+  //     } else {
+  //       // commit('loginfailed', err)
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //     if (error && error.response.data.msg === "Token has expired") {
+  //       localStorage.removeItem("authenticated");
+  //       router.push("/");
+  //     }
+  //   }
+  // },
 
   async getActivity({ commit }) {
     let response = await axios.get("/recent_activities");
