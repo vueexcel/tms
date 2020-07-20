9913064090562ee76d02cdbf5acf4cf923d5c5cd
ashwani <ashwani@excellencetechnologies.info>
2020-07-20 23:37:27 +0530

    fixed test case for input type for username, password , checkbox

--- src/components/common/input.test.js
+++ src/components/common/input.test.js
@@ -5,46 +5,38 @@ import userEvent from '@testing-library/user-event'
 import Input from "./input.vue"
 
 test("input for text", async () => {
-    let mockCallback = jest.fn((x) => x)
 
     const { getByPlaceholderText } = await render(Input, {
         propsData: {
           type: 'text',
           id:"123",
-          placeholder:"placeholder"
+          placeholder:"Username"
         },
       })
 
-    let input = getByPlaceholderText("placeholder")
+    let input = getByPlaceholderText("Username")
     expect(input).toBeInTheDocument()
-
     expect(input).toHaveAttribute("type", "text")
-
-    await userEvent.type(input, "123")
-
-    // expect(mockCallback).toBeCalledWith("123")
-
-    // expect(mockCallback).toBeCalledTimes(3)
-
-
+    expect(input.value).toBe("")
+    await fireEvent.update(input, 'Bob')
+    expect(input.value).toBe("Bob")
 })
 
 test("input type for password", async () => {
-
     const { container } =  await render(Input, {
         propsData: {
           type: 'password',
           id:"123",
-          placeholder:"placeholder"
+          placeholder:"password"
         }
       })
 
     let element = container.querySelector("input")
-
     expect(element).toBeInTheDocument()
-
     expect(element).toHaveAttribute("type", "password")
-
+    expect(element.value).toBe("")
+    await fireEvent.update(element, 'Password')
+    expect(element.value).toBe("Password")
 }) 
 
 test("input type for checkbox", async () => {
@@ -58,9 +50,27 @@ test("input type for checkbox", async () => {
       })
  
     let element = container.querySelector("input")
-
     expect(element).toBeInTheDocument()
-
     expect(element).toHaveAttribute("type", "checkbox")
+    expect(element.checked).toBe(false)
+    await fireEvent.update(element, true)
+    expect(element.checked).toBe(true)
+}) 
 
+test("input type for number", async () => {
+
+  const { container } =  await render(Input, {
+      propsData: {
+        type: 'number',
+        id:"123",
+        placeholder:"placeholder"
+      }
+    })
+
+  let element = container.querySelector('input')
+  expect(element).toBeInTheDocument()
+  expect(element).toHaveAttribute("type", "number")
+  expect(element.value.number).toBe()
+  await fireEvent.update(element,123)
+  expect(element.value.number).toBe(123)
 }) 
